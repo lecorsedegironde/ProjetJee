6470c8479e12de379ce3a8ed687105e6501838d7
lukijo <mickae56@yahoo.fr>
2018-04-05 15:13:01 +0200

    Servlet inscription

---
+++ src/main/java/fr/grp404/projetjee/web/servlet/RegisterServlet.java
@@ -0,0 +1,28 @@
+package fr.grp404.projetjee.web.servlet;
+
+import com.google.inject.Inject;
+import fr.grp404.projetjee.persistence.dao.UserDao;
+
+import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Singleton
+public class RegisterServlet extends HttpServlet{
+
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO des choses
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO des choses
+    }
+}
